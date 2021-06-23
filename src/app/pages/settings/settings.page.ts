aaaff9e8b4255b8d1d306a280f7893ff9f7ca587
Ali <tp047366@mail.apu.edu.my>
2021-06-24 07:32:06 +0800

    added datepicker to settings page

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-settings',
@@ -11,13 +12,20 @@ import { ModalController } from '@ionic/angular';
 })
 export class SettingsPage implements OnInit {
 
+  birthDate: any;
+  userID: string;
+
   constructor(
     public modalCtrl: ModalController,
+    private afs: AngularFirestore,
     private auth: AuthService,
     private router: Router,
   ) { }
 
   ngOnInit() {
+    this.auth.user$.subscribe(user => {
+      this.userID = user.userID;
+    })
   }
 
    logout() {
@@ -30,5 +38,11 @@ export class SettingsPage implements OnInit {
       cssClass: 'my-custom-class'
     });
     return await modal.present();
+  }
+
+  submit() {
+    this.afs.collection('user').doc(this.userID).set({
+      'birthDate': this.birthDate
+    }, {merge : true})
   }
 }
